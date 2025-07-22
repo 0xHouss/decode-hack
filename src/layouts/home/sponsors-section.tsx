2ccff5bd7d60e65dc613cbc5ab1b0eb3e5b4cbf5
0xHouss <tkthoussam@gmail.com>
2025-07-22 20:13:43 +0100

    feat: add hover cards to sponsors

--- src/layouts/home/sponsors-section.tsx
+++ src/layouts/home/sponsors-section.tsx
@@ -1,35 +1,58 @@
 import Section from "@/components/section";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { sponsors } from "@/lib/config";
 import Image from "next/image";
+import Link from "next/link";
+
+export function SponsorCard({ sponsor }: { sponsor: typeof sponsors[0] }) {
+  return (
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <Link href={sponsor.url} target="_blank" rel="noopener noreferrer">
+          <Image
+            src={sponsor.logo}
+            alt={`Sponsor ${sponsor.name} Logo`}
+            width={200}
+            height={200}
+            className="object-contain"
+          />
+        </Link>
+      </HoverCardTrigger>
+      <HoverCardContent sideOffset={-40} className="w-90">
+        <div className="flex justify-between gap-4">
+          <div className="space-y-1">
+            <h4 className="font-bold">{sponsor.name}</h4>
+            <p className="text-xs">{sponsor.description}</p>
+          </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  )
+}
 
 export default function Sponsors() {
   return (
     <Section id="sponsors" className="flex flex-col gap-8 items-center max-w-6xl">
       <h2 className="text-5xl bg-gradient-to-r from-[#E9FDB0] to-[#4E941A] bg-clip-text text-transparent">Our Sponsors</h2>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center">
-        <Image
-          src="/sponsor-1.png"
-          alt="Sponsor 1"
-          width={150}
-          height={100}
-          className="object-contain"
-        />
-        {/* <Separator orientation="vertical" className="bg-gradient-to-b from-[#E3E812]/0 via-[#E3E812]/50 to-[#E3E812]/0" /> */}
-        <Image
-          src="/sponsor-2.png"
-          alt="Sponsor 2"
-          width={150}
-          height={150}
-          className="object-contain"
-        />
-        {/* <Separator orientation="vertical" className="bg-gradient-to-b from-[#E3E812]/0 via-[#E3E812]/50 to-[#E3E812]/0" /> */}
-        <Image
-          src="/sponsor-3.png"
-          alt="Sponsor 3"
-          width={150}
-          height={150}
-          className="object-contain"
-        />
+      <div className="flex flex-col md:flex-row md:gap-8 items-center">
+        {sponsors.map((sponsor, idx) => (
+          <div key={sponsor.name} className="flex flex-col md:flex-row items-center gap-4 md:gap-8">
+
+            <SponsorCard sponsor={sponsor} />
+
+            {idx < sponsors.length - 1 && (
+              <div
+                className="bg-gradient-to-r md:bg-gradient-to-b h-0.5 w-30 md:h-30 md:w-0.5 from-[#E3E812]/0 via-[#E3E812]/50 to-[#E3E812]/0"
+                key={`separator-${idx}`}
+              />
+            )}
+          </div>
+        ))}
       </div>
     </Section>
   )
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -42,3 +42,24 @@ export const sections = [
 ]
 
 export const registrationEndDate = new Date("2025-07-27T19:00:00Z"); // Set your target date here
+
+export const sponsors = [
+  {
+    name: "EDZEN",
+    description: "EDZEN est une entreprise algérienne engagée pour l’environnement, l’éducation et l’entrepreneuriat vert. Elle accompagne la création de green business à travers des formations, des ateliers et des expériences d’apprentissage accessibles. EDZEN vise à transformer les consommateurs en producteurs responsables, tout en valorisant les savoir-faire artisanaux et les métiers durables.",
+    logo: "/edzen-logo.png",
+    url: "https://www.instagram.com/edzen.dz/"
+  },
+  {
+    name: "DZHOSTER",
+    description: "DZHOSTER, marque du groupe AYRADE, accompagne depuis plus de 7 ans les particuliers exigeants — startups, étudiants, agences, freelances. Avec plus de 4000 clients à son actif, leur réseau s’étend à travers toute l’Algérie, porté par une équipe engagée et des partenaires de confiance.",
+    logo: "/dz-hoster-logo.png",
+    url: "https://www.dzhoster.com/"
+  },
+  {
+    name: "Well Career",
+    description: "Well Career est un organisme qui vise a sélectionner, développer et accompagner les professionnels pour qu'ils s'épanouissent dans leur parcours. Grâce à des processus de Recrutement Rigoureux, des Formations Ciblées et un Accompagnement Personnalisé, Well Career aident chaque talent à trouver sa place dans un environnement où il pourra exceller.",
+    logo: "/well-career-logo.png",
+    url: "https://career.wellpharmagroup.com/"
+  },
+] 
