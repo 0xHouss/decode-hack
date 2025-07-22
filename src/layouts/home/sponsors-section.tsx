62fd1a8a2a20ca221d7bd99a167ddfea619febaa
0xHouss <tkthoussam@gmail.com>
2025-07-22 12:21:41 +0100

    add 2nd sponsor and remove 4th

--- src/layouts/home/sponsors-section.tsx
+++ src/layouts/home/sponsors-section.tsx
@@ -6,7 +6,7 @@ export default function Sponsors() {
     <Section id="sponsors" className="flex flex-col gap-8 items-center max-w-6xl">
       <h2 className="text-5xl bg-gradient-to-r from-[#E9FDB0] to-[#4E941A] bg-clip-text text-transparent">Our Sponsors</h2>
 
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 items-center">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-center">
         <Image
           src="/sponsor-1.png"
           alt="Sponsor 1"
@@ -26,14 +26,6 @@ export default function Sponsors() {
         <Image
           src="/sponsor-3.png"
           alt="Sponsor 3"
-          width={150}
-          height={150}
-          className="object-contain"
-        />
-        {/* <Separator orientation="vertical" className="bg-gradient-to-b from-[#E3E812]/0 via-[#E3E812]/50 to-[#E3E812]/0" /> */}
-        <Image
-          src="/sponsor-4.png"
-          alt="Sponsor 4"
           width={150}
           height={150}
           className="object-contain"
