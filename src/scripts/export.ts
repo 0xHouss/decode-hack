1d0e67a36b3fa83c2c61d5add088a5a8ef82fa3e
0xHouss <tkthoussam@gmail.com>
2025-07-23 12:27:56 +0100

    create export script

---
+++ src/scripts/export.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/lib/prisma';
+import { formatDate } from '@/lib/utils';
+import { Submission } from '@prisma/client';
+import { writeFileSync } from 'fs';
+
+async function main() {
+  try {
+    const data = await prisma.submission.findMany({
+      orderBy: {
+        submittedAt: 'asc',
+      },
+    });
+
+    if (!data.length)
+      return console.log('No data found.');
+
+    const headers: (keyof Submission)[] = [
+      "submittedAt",
+      "firstName",
+      "lastName",
+      "email",
+      "phone",
+      "birthDate",
+      "NIN",
+      "institution",
+      "enrollmentYear",
+      "major",
+      "matricule",
+      "teamName",
+      "availability",
+      "prevExperience",
+      "prevExperienceDetails",
+      "motivation",
+      "skills",
+      "github",
+      "linkedin",
+      "portfolio",
+      "kaggle"
+    ]
+
+    const rows = data.map(row => headers.map(h => {
+      if (h === 'submittedAt') return formatDate(row[h]);
+      return JSON.stringify(row[h] ?? '')
+    }).join(','));
+
+    const csv = [headers.join(','), ...rows].join('\n');
+
+    writeFileSync('export.csv', csv);
+    console.log('✅ Exported to export.csv');
+  } catch (error) {
+    console.error('❌ Failed to export:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main().catch((error) => {
+  console.error('Error executing export script:', error);
+  process.exit(1);
+})
